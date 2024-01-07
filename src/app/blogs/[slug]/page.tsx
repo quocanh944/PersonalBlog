e03e474d65411365a3b5a8acba2a77dad06fff48
Nguyễn Quốc Anh (Han) <quocanh944@gmail.com>
2024-01-07 10:58:52 +0700

    Fix style

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -6,6 +6,8 @@ import { NotionRenderer } from "@notion-render/client";
 import hljsPlugin from "@notion-render/hljs-plugin";
 import { notFound } from "next/navigation";
 import { notion } from "@/lib/notion";
+import { createBlockRenderer } from "@notion-render/client";
+import type { CalloutBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export default async function DetailBlog({ params }: { params: { slug: string } }) {
 
@@ -15,8 +17,24 @@ export default async function DetailBlog({ params }: { params: { slug: string } 
         notFound()
     }
 
+    const calloutBlockRender = createBlockRenderer<CalloutBlockObjectResponse>(
+        'callout', async (data, render) => {
+            let emoji = '';
+            if (data.callout.icon?.type === 'emoji') {
+                emoji = data.callout.icon.emoji;
+            }
+
+            return `
+            <div class="px-4 py-3 border border-neutral-200 bg-neutral-50 rounded text-sm flex items-center my-3 bg-slate-200">
+                <div class="flex items-center w-4 mr-4">${emoji}</div>
+                <div class="w-full callout">${await render.render(...data.callout.rich_text)}</div>
+            </div>`;
+        }
+    );
+
     const renderer = new NotionRenderer({
         client: notion,
+        renderers: [calloutBlockRender]
     });
 
     renderer.use(hljsPlugin({}));
